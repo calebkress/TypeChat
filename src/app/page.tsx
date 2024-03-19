96b93f32a2db8e3f9da763ea70edec60274f2e17
Caleb Kress <calebkress@gmail.com>
2024-03-19 01:06:09 -0500

    basic user mockup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { db } from "@/lib/db"
 
 export default async function Home() {
-  await db.set("hello", "hello")
 
-  return <div className="text-red-500">hello world</div>
+  interface UserMock {
+    name: string
+  }
+
+  const user: UserMock = {
+    
+  }
+
 }
