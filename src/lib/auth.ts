77bb09207567dd770113f5f8cc8b29e53d1b16c5
Caleb Kress <calebkress@gmail.com>
2024-03-19 00:36:22 -0500

    authentication setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,8 @@
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: 
+}
